12ddb252279b4002a40c45698680004276cbdc9e
Thomas Schmitt <t.e.schmitt@posteo.de>
2025-08-22 16:42:55 +0200

    feat: minimal lora setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,16 @@
+#include <zephyr/device.h>
+#include <zephyr/drivers/lora.h>
+#include <errno.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/drivers/uart.h>
 
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-             "Console device is not ACM CDC UART device");
+#define DEFAULT_RADIO_NODE DT_ALIAS(lora0)
+BUILD_ASSERT(DT_NODE_HAS_STATUS_OKAY(DEFAULT_RADIO_NODE),
+             "No default LoRa radio specified in DT");
 
 int main(void)
 {
-    const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-    uint32_t dtr = 0;
+    const struct device *lora_dev;
 
-    /* Poll if the DTR flag was set */
-    while (!dtr)
-    {
-        uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-        /* Give CPU resources to low priority threads. */
-        k_sleep(K_MSEC(100));
-    }
-
-    while (1)
-    {
-        printk("Hello World! %s\n", CONFIG_ARCH);
-        k_sleep(K_SECONDS(1));
-    }
+    return 0;
 }
