f429e68e7354d314269b927e9693066750db752d
Thomas Schmitt <t.e.schmitt@posteo.de>
2026-04-02 08:51:21 +0000

    feat: add TS_MSG_CONFIG_SET and TS_MSG_CONFIG_ACK message types

--- src/messages/messages.h
+++ src/messages/messages.h
@@ -11,10 +11,15 @@
 
 #include "routing/routing.h"
 
+/** @brief Maximum key length for config set/ack messages. */
+#define TS_MSG_CONFIG_KEY_MAX 32
+
 /** @brief Message type discriminator. */
 typedef enum {
     TS_MSG_TELEMETRY = 0,
     TS_MSG_NODE_STATUS = 1,
+    TS_MSG_CONFIG_SET = 2,
+    TS_MSG_CONFIG_ACK = 3,
 } ts_msg_type_t;
 
 /** @brief Node status codes. */
@@ -38,6 +43,19 @@ struct ts_msg_node_status {
     ts_status_t status;
 };
 
+/** @brief Config set request (key-value pair to apply). */
+struct ts_msg_config_set {
+    char key[TS_MSG_CONFIG_KEY_MAX];
+    int32_t value;
+};
+
+/** @brief Config ack response (result of applying a config set). */
+struct ts_msg_config_ack {
+    char key[TS_MSG_CONFIG_KEY_MAX];
+    int32_t value;
+    int32_t result;
+};
+
 /** @brief Outgoing message with route header and typed payload. */
 struct ts_msg_lora_outgoing {
     struct ts_route_header route;
@@ -45,6 +63,8 @@ struct ts_msg_lora_outgoing {
     union {
         struct ts_msg_telemetry telemetry;
         struct ts_msg_node_status node_status;
+        struct ts_msg_config_set config_set;
+        struct ts_msg_config_ack config_ack;
     } data;
 };
 
